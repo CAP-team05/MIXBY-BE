1d2e33666368171270f6304c920e1cc2ef6714b1
ahn <ahnjh05141@naver.com>
2024-10-06 23:08:12 +0900

    docs: alcohol info added to drinks json file

--- createfile_drink.py
+++ createfile_drink.py
@@ -1,23 +1,32 @@
-import my_modules.getinfo, json
+from my_modules import getinfo
+import searchimage
+import json
 
-urls = my_modules.getinfo.geturls('origin_data/urls.txt')
+urls = getinfo.geturls('old_data/urls.txt')
 
 drinks = []
 
+cnt = 1
 for url in urls:
-    code = my_modules.getinfo.getcode(url)
-    name = my_modules.getinfo.getnameKOR(url)
+    code = getinfo.getcode(url)
+    name = getinfo.getnameKOR(url)
     volume = name.split('(')[-1]
     name = name.replace('('+volume, '').strip(' ')
     volume = volume.strip(' ').strip(')')
+
+    al = searchimage.getalcohol(code)
 
     drink = {}
     drink['code'] = code
     drink['name'] = name
     drink['volume'] = volume
+    drink['alcohol'] = al
     drinks.append(drink)
 
-with open('new_data\\drinks.json', 'w', encoding='utf-8') as f:
+    print(cnt)
+    cnt += 1
+
+with open('new_data/drinks.json', 'w', encoding='utf-8') as f:
     json.dump(drinks, f, ensure_ascii = False, indent=4)
 
 print('\nfile successfully created!\n')
--- searchimage.py
+++ searchimage.py
@@ -1,9 +1,44 @@
-import glob, pytesseract
+from my_modules import gettype
+import cv2, pytesseract, json
 
-# tesseract 가 윈도우 지원이 깔끔해서 윈도우로 해야할듯???
+# json file 읽기
+with open('new_data/drinks.json', 'r', encoding='UTF-8') as json_read :
+    json_str = json.load(json_read)
 
+new_list = []
 cnt = 0
-fs = glob.glob('images_merged/*_merged.jpg')
-for f in fs[:5]:
-    text = pytesseract.image_to_string(f, lang='kor+eng')
-    print(text)
+for j in json_str[:10]:
+    code = j['code']
+    f = "images_merged/"+code+"_merged.jpg"
+    image = cv2.imread(f)
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    lines = pytesseract.image_to_string(rgb_image, lang='kor', config='--psm 6')
+    lines = lines.split('\n')
+
+    cnt += 1
+    #print(cnt)
+
+    c = j['code']
+    n = j['name']
+    v = j['volume']
+    a = j['alcohol']
+    t = gettype.searchtypeKOR(n)
+
+    print(cnt, n)
+    print(t)
+    print()
+
+    new_dict = {}
+    new_dict['code'] = c
+    new_dict['name'] = n
+    new_dict['type'] = t
+    new_dict['volume'] = v
+    new_dict['alcohol'] = a
+    
+    new_list.append(new_dict)
+
+
+# with open('new_data/drinks_copy.json', 'w', encoding='utf-8') as f:
+#     json.dump(new_list, f, ensure_ascii = False, indent=4)
+
+# print('\nfile successfully created!\n')
