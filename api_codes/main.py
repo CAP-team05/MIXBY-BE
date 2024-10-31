271567f1449d6925f06526ff661c52e103d98b0d
JaeHyun <jaehyuns05141@gmail.com>
2024-11-01 00:49:26 +0900

    feat: api error solved

--- api_codes/main.py
+++ api_codes/main.py
@@ -1,28 +1,66 @@
 from flask import Flask, jsonify, render_template
 
-import get
+import json
+import get_drink, get_recipe
 
 app = Flask(__name__)
 
 @app.route('/')  # '/' 경로 접속 시 start 실행 (라우팅 이라고 부름)
 def start():  # 함수의 이름은 중복만 되지 않으면 됨
-    return "믹스비 어플 개발중..."
+    return render_template('main.html')
 
-@app.route('/product/<code>')
-def barcode(code):
-    if code == "all": return get.getall()
-    info = get.getinfo(code)
-    return info
+# show all drink
+@app.route('/drink/all')
+def drinks():
+    return app.response_class(
+        response=json.dumps(get_drink.getalldrinks(), indent=4),
+        mimetype='application/json'
+    )
+# search drinks including <code> at name or code
+@app.route('/drink/<code>')
+def drink(code):
+    info = get_drink.getdrink(code)
+    return app.response_class(
+        response=json.dumps(info, indent=4),
+        mimetype='application/json'
+    )
 
-@app.route('/recipe/<code>')
-def recipe(code):
-    if code == "all": return "구현 예정!"
-    info = "구현 예정!"
-    return info
+# show all recipe
+@app.route('/recipe/all')
+def all_recipes():
+    return app.response_class(
+        response=json.dumps(get_recipe.getallrecipes(), indent=4),
+        mimetype='application/json'
+    )
+# show recipe image
+@app.route('/recipe/image/<code>')
+def recipe_image(code=None):
+    return render_template('image_recipe.html', code=code)
 
-@app.route('/yee')
-def yee():
-    return render_template('myimage.html')
+# search recipes by name
+@app.route('/recipe/name/<name>')
+def recipe_name(name):
+    info = get_recipe.getrecipe_byname(name)
+    return app.response_class(
+        response=json.dumps(info, indent=4),
+        mimetype='application/json'
+    )
+# search recipes by code
+@app.route('/recipe/code/<code>')
+def recipe_code(code):
+    info = get_recipe.getrecipe_bycode(code)
+    return app.response_class(
+        response=json.dumps(info, indent=4),
+        mimetype='application/json'
+    )
+# search recipes by ingredients
+@app.route('/recipe/ing/<ing>')
+def recipe_ing(ing):
+    info = get_recipe.getrecipe_byings(ing)
+    return app.response_class(
+        response=json.dumps(info, indent=4),
+        mimetype='application/json'
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2222)  # app 실행
