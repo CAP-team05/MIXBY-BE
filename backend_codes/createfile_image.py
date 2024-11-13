e21a998c9d3fc2b886b4c5e0ef6c9417072f41b0
JaeHyun <jaehyuns05141@gmail.com>
2024-11-13 15:57:35 +0900

    feat: drink img files

--- backend_codes/createfile_image.py
+++ backend_codes/createfile_image.py
@@ -1,7 +1,7 @@
 import my_modules.getinfo
 import os, time
 
-urls = my_modules.getinfo.geturls("old_data/urls.txt")
+urls = my_modules.getinfo.geturls("backend_codes/old_data/urls.txt")
 
 cnt = 0
 for url in urls:
@@ -11,5 +11,5 @@ for url in urls:
 
     for i in range(0,len(links)):
         url = links[i]
-        os.system("curl "+url+" > "+os.path.join('images\\', '{}_{}.jpg'.format(code,i)))
+        os.system("curl "+url+" > "+os.path.join('backend_codes/images', '{}_{}.jpg'.format(code,i)))
         cnt += 1
