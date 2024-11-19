7251bc6f23c5263570c2091e7cf2295c4ca0369b
JaeHyun <jaehyuns05141@gmail.com>
2024-11-19 20:31:27 +0900

    feat: recipe code

--- api_codes/get_recipe.py
+++ api_codes/get_recipe.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
 import json
 
-with open('api_codes/json_files/recipes.json', 'r', encoding='UTF-8') as json_read :
+with open('api_codes/json_files/allRecipes.json', 'r', encoding='UTF-8') as json_read :
     all_recipes = json.load(json_read, object_pairs_hook=OrderedDict)
 
 
