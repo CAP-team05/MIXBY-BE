836b304b30b921ca79a8d29c0f930de1465bdb1b
JaeHyun <jaehyuns05141@gmail.com>
2024-12-01 19:01:49 +0900

    feat: 리큐르 코드 분리

--- api_codes/get_recipe.py
+++ api_codes/get_recipe.py
@@ -18,21 +18,32 @@ def search_byname(name):
 def search_bycode(code):
     temp_list = []
     for j in all_recipes:
-        if code == j["code"] and j not in temp_list: temp_list.append(j)
-    temp_list.insert(0, "Total result found : {}".format(len(temp_list)))
-    return temp_list
+        if code == j["code"]: return j
+    return "no result found"
 
 def search_byings(codes):
-    code_list = list(map(''.join, zip(*[iter(codes)]*3)))
     temp_list = []
-    for input_code in code_list:
-        for j in all_recipes:
-            recipe_code = j["code"]
-            if input_code in recipe_code:
-                cnt = 0
-                for c in code_list:
-                    if c in recipe_code: cnt+=1
-                j["have"] = "{}/{}".format(cnt, int(len(j["code"])/2))
-                if j not in temp_list: temp_list.append(j)
-    
+
+    input_codes = list(map(''.join, zip(*[iter(codes)]*3)))
+    print(input_codes)
+
+    for recipe in all_recipes:
+        recipe_codes = list(map(''.join, zip(*[iter(recipe["code"])]*3)))
+        cnt = 0
+        for i in input_codes:
+            if i in recipe_codes:
+                cnt += 1
+                print(cnt)
+                
+        if cnt > 0:
+            tempDict = {}
+            tempDict["code"] = recipe["code"]
+            tempDict["english_name"] = recipe["english_name"]
+            tempDict["korean_name"] = recipe["korean_name"]
+            tempDict["tag1"] = recipe["tag1"]
+            tempDict["tag2"] = recipe["tag2"]
+            tempDict["have"] = "{}/{}".format(cnt, len(recipe_codes))
+            
+            temp_list.append(tempDict)
+
     return temp_list
---
+++ api_codes/make_recipe_code.py
@@ -0,0 +1,51 @@
+from collections import OrderedDict
+import json
+
+with open('api_codes/json_files/oldRecipes.json', 'r', encoding='UTF-8') as json_read :
+    allRecipes = json.load(json_read, object_pairs_hook=OrderedDict)
+
+
+with open('api_codes/json_files/allIngredients.json', 'r', encoding='UTF-8') as json_read :
+    allIngredients = json.load(json_read, object_pairs_hook=OrderedDict)
+
+
+resultList = []
+
+for recipe in allRecipes:
+    tempDict = {}
+
+    tempDict["english_name"] = recipe["english_name"]
+    tempDict["korean_name"] = recipe["korean_name"]
+
+
+    ingList = []
+    for recipeIng in recipe["ingredients"]:
+        for ing in allIngredients:
+            ingDict = {}
+            if recipeIng["name"] == ing["name"]:
+                ingDict["name"] = ing["name"]
+                ingDict["code"] = ing["code"]
+                ingDict["amount"] = recipeIng["name"]
+                ingDict["unit"] = recipeIng["name"]
+                ingList.append(ingDict)
+
+    code = ""
+    for ing in ingList:
+        code = code + ing["code"]
+
+    tempDict["code"] = code
+    tempDict["ingredients"] = ingList
+
+    
+
+    tempDict["instructions"] = recipe["instructions"]
+    tempDict["tag1"] = recipe["tag1"]
+    tempDict["tag2"] = recipe["tag2"]
+
+    resultList.append(tempDict)
+
+
+with open('api_codes/json_files/allRecipes.json', 'w', encoding='utf-8') as f:
+    json.dump(resultList, f, ensure_ascii = False, indent=4)
+
+print('\nfile successfully created!\n')
