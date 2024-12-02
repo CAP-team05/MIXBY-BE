74e728815317e1a0e6c5292c01c9df7f83d21e53
JaeHyun <jaehyuns05141@gmail.com>
2024-12-02 17:01:45 +0900

    feat: return all ingredients

--- api_codes/get_recipe.py
+++ api_codes/get_recipe.py
@@ -4,10 +4,6 @@ import json
 with open('api_codes/json_files/allRecipes.json', 'r', encoding='UTF-8') as json_read :
     all_recipes = json.load(json_read, object_pairs_hook=OrderedDict)
 
-
-def getallrecipes():
-    return all_recipes
-
 def search_byname(name):
     temp_list = []
     for j in all_recipes:
