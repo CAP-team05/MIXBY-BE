31d7855b018dff50a1889b619123eb3183b4179b
ahnjh05141 <jaehyuns05141@gmail.com>
2024-12-09 00:35:57 +0900

    feat: getting random recipe for testing

--- api_codes/runServer.py
+++ api_codes/runServer.py
@@ -3,6 +3,7 @@ from functools import wraps
 
 import json
 import get_drink, get_recipe, get_ingredients, get_persona, get_weather, get_recommend, get_challenges
+import random
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False  # 한글이 깨지지 않도록 설정
@@ -91,6 +92,14 @@ def recipe_with(codes):
         mimetype='application/json'
     )
 
+# return random recipe for testing
+@app.route('recipe/random')
+def recipe_random():
+    info = get_recipe.all_recipes[random.randint(0, len(all_recipes))]
+    return app.response_class(
+        response=json.dumps(info, indent=4),
+        mimetype='application/json'
+    )
 
 # search ingredients by code
 @app.route('/ing/with=<codes>')
